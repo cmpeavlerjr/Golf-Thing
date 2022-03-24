8b2a9f9e8b2180e0ae9bf1e844800c937e7a9336
cmpeavlerjr <102272635+cmpeavlerjr@users.noreply.github.com>
2022-03-23 22:50:38 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,94 @@
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import background1 from './Images/ANGC.jpg';
+import React, { useState, useEffect } from 'react';
+import Table from 'react-bootstrap/Table'
+import Managers from './managers.json'
+
+function Manager(props) {
+  const [showToggle, setToggle] = useState(false);
+  return <tbody>
+    <tr onClick={() => { setToggle(!showToggle);}}>
+      <th scope="row">{props.row}</th>
+      <td>{props.name}</td>
+      <td >{props.score}</td>
+    </tr>
+    <tr style={(showToggle) ? {} : {display:"none"}}>
+      <td colSpan={3}>      
+        <Table striped borderless hover>
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">Player Name</th>
+                <th scope="col">Score</th>
+              </tr>
+            </thead>
+            {props.players.map((player) =>
+              <tr>
+                <td>{player.Name}</td>
+                <td>{player.Score}</td>
+              </tr>
+            )}
+        </Table>
+      </td>
+    </tr>
+  </tbody>
+}
+
+function App(props) {
+  const myStyle={
+      height:'100vh',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      // backgroundImage: `url(${background1})`
+    };
+
+  const listItems = Managers.managers.map((manager) =>
+    <Manager name={manager.name} row={manager.row} score={manager.score} players={manager.players} />
+  );
+
+
+  var sectionStyle = {
+    width: "100%",
+    height: "400px",
+    backgroundColor: 'white'
+  };
+
+  // useEffect(() => {
+  //   fetch(url, {
+  //     mode: 'cors',
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     }})
+  //   .then((response) => {
+  //     document.title = `Get fucked newb`;
+  //     setCount("fucking shit ass " + response.status + "ass ass ass");
+  //     return response.json();
+  //   })
+  //   .then((json) => {
+  //     document.title = `Get fucked newb`;
+  //     setCount(json);
+  //   });
+  // });
+
+
+  return (
+    <div style={myStyle}>
+      <Container className="App">
+          <div>{props.data.id}</div>
+          <Table striped bordered hover style={sectionStyle}>
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">Position</th>
+                <th scope="col">Manager Name</th>
+                <th scope="col">Score</th>
+              </tr>
+            </thead>
+              {listItems}
+          </Table>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
